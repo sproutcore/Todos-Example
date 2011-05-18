6f12012acdb1bd5dd5beefca96cdf193984d2784
Piotr Sarnacki <drogus@gmail.com>
2011-05-18 14:55:17 +0200

    Use Todos.TextField that extends SC.TextField

--- apps/todos/views/text_field.js
+++ apps/todos/views/text_field.js
@@ -1,33 +1,5 @@
-Todos.TextFieldView = SC.TemplateView.extend(SC.TextFieldSupport, {
-
-  classNames: ['text-field-view'],
-
-  /* The 'name' attribute of the input. */
-  name: '',
-
-  /* The 'placeholder' attribute of the input. */
+Todos.TextField = SC.TextField.extend({
   placeholder: '',
-
-  /* The 'title' attribute of the input. */
-  title: '',
-
-  /* The 'type' attribute of the input. */
   type: 'text',
-
-  // These could be made into some generic displayProperties method like in theming.js
-  // These could also check for a localize property like many controls do
-  displayTitle: function() {
-    var title = this.get('title');
-
-    return title.loc();
-  }.property('title').cacheable(),
-
-  displayPlaceholder: function() {
-    var placeholder = this.get('placeholder');
-
-    return placeholder.loc();
-  }.property('placeholder').cacheable(),
-
-  template: SC.Handlebars.compile('<input {{bindAttr name="name" type="type" title="displayTitle" placeholder="displayPlaceholder"}}/>')
-
+  template: SC.Handlebars.compile('<input {{bindAttr  type="type" placeholder="placeholder"}}/>')
 });
